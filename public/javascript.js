98996fd036c62477d96429fd5d29625364aea07f
Sudharshan Shanmugasundaram <sudharshanbasketball@gmail.com>
2018-07-28 11:12:27 +0530

    Simple Rest Api Application

--- public/javascript.js
+++
@@ -1,9 +0,0 @@
-// var li = document.querySelectorAll('.dropdown-menu a');
-// console.log(li);
-// var company;
-// li.forEach(function(a){
-//   a.addEventListener('click',function(e){
-//     console.log(e.target.innerHTML);
-//     company = e.target.innerHTML;
-//   });
-// });
